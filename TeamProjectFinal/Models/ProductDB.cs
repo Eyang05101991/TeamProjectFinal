d2188a9ff6f7194d5d99075d19162a04dcbca69a
Eyang05101991 <31074569+Eyang05101991@users.noreply.github.com>
2018-02-11 21:34:56 -0800

    added two new classes to the model called SubscribeDB that will have all of the methods for subscribing like canceling subscription etc.
    created a class called ProductDB that will have all the methods for handling the products like update product, delete product etc.

---
+++ TeamProjectFinal/Models/ProductDB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeamProjectFinal.Models
+{
+    public static class ProductDB
+    {
+        public static List<Product> GetAllProducts()
+        {
+            TeamProjectFinalDB db = new TeamProjectFinalDB();
+            List<Product> prods = db.Products.ToList();
+            return prods;
+        }
+
+        public static void AddProduct()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void DeleteProduct()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void UpdateProduct()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TeamProjectFinal/Models/SubscribeDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeamProjectFinal.Models
+{
+    public static class SubscribeDB
+    {
+        public static void Subsribe(Subscriber s)
+        {
+            TeamProjectFinalDB db = new TeamProjectFinalDB();
+            db.Subscribers.Add(s);
+            db.SaveChanges();
+
+        }
+    }
+}
